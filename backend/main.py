685e32b8ef9b860e6926a7d9870fa8265f65a707
it24100163 <it24100163@my.sliit.lk>
2026-03-01 16:09:37 +0530

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,28 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 from database import engine, get_db, Base
-from models import * 
+from models import *
 from APIs import cart, orders
-from APIs import feedback ##----
-
+from APIs import feedback
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Ransara Supermarket API")
 
+#  CORS: allow frontend (React) to call backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for demo. In real project, put your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(cart.router)
 app.include_router(orders.router)
-app.include_router(feedback.router) ##----
+app.include_router(feedback.router)
 
 @app.get("/")
 def root():
@@ -22,7 +31,7 @@ def root():
 @app.get("/test-db")
 def test_database_connection(db: Session = Depends(get_db)):
     try:
-        result = db.execute(text("SELECT 1"))
+        db.execute(text("SELECT 1"))
         return {"status": "success", "message": "Successfully connected to PostgreSQL!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Database connection failed: {str(e)}")
