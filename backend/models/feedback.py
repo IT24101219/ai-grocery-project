064bc421831d720475a8f3cb320b55079fd1b84b
it24100163 <it24100163@my.sliit.lk>
2026-03-04 16:34:14 +0530

    Add files via upload

--- backend/models/feedback.py
+++ backend/models/feedback.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean
+from sqlalchemy import Column, DateTime, Integer, String, Text, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from database import Base
+from sqlalchemy.sql import func
+from datetime import datetime
 
 class Feedback(Base):
     __tablename__ = "feedbacks"
@@ -8,7 +10,12 @@ class Feedback(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_name = Column(String, nullable=False)
     message = Column(Text, nullable=False)
+    product_name = Column(String, nullable=False) 
     rating = Column(Integer, nullable=False)
     reply = Column(Text, nullable=True)
     role = Column(String, default="user")  # user or admin
     offensive = Column(Boolean, default=False)  # Flag for offensive content
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+    replied_at = Column(DateTime(timezone=True), nullable=True)
